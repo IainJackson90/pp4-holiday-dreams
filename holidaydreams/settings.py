e472c80f5b454f438085048dfa6b0a29f1c4d63e
Iain Jackson <iainjackson90@gmail.com>
2024-04-13 12:56:10 +0000

    Add: Post Model

--- holidaydreams/settings.py
+++ holidaydreams/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=*1^fr592+&w+_%ppu72h+!l=$+#q-3+xpc^oca9bzqpsvn#l)'
+# SECRET_KEY = 'django-insecure-=*1^fr592+&w+_%ppu72h+!l=$+#q-3+xpc^oca9bzqpsvn#l)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -90,6 +91,12 @@ WSGI_APPLICATION = 'holidaydreams.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.heroko.com",
+    "https://*.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
