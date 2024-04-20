461e20f700be4e4b544b80afe2bbcd349273605a
Iain Jackson <iainjackson90@gmail.com>
2024-04-20 13:57:38 +0000

    Add: style.css basick css

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,4 @@ class PostList(generic.ListView):
     # queryset = Post.objects.all()
     # template_name = "post_list.html"
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 9
