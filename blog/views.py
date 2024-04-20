fe37643593d7878f098e8566ba90980903addb95
Iain Jackson <iainjackson90@gmail.com>
2024-04-20 13:17:00 +0000

    Add: base.html and index.htm with basick layout of content

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    # queryset = Post.objects.filter(status=1)
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.all()
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
