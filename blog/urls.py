ffaad43d6015a08ad181581e4b0f885124875d2c
Iain Jackson <iainjackson90@gmail.com>
2024-04-30 20:39:46 +0000

    Update: Cancel button ctyling and add like icon to index page

--- blog/urls.py
+++ blog/urls.py
@@ -3,24 +3,15 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-     path(
-            'post_create/',
-            views.CreatePost.as_view(),
-            name='post_create'
-      ),
-      path(
-            'post_update/<slug:slug>',
-            views.PostUpdate.as_view(),
-            name='update_post'
-      ),
-       path(
-            'delete_post/<slug:slug>',
-            views.DeletePost.as_view(),
-            name='delete_post'
-      ),
+    path('post_create/',views.CreatePost.as_view(),
+         name='post_create'),
+    path('post_update/<slug:slug>',views.PostUpdate.as_view(),
+         name='update_post'),
+    path('delete_post/<slug:slug>',views.DeletePost.as_view(),
+         name='delete_post'),
     path('<slug:slug>/', views.post_detail, name="post_detail"),
     path('<slug:slug>/edit_comment/<int:comment_id>',
-        views.comment_edit, name='comment_edit'),
+         views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
     path('like/<slug:slug>', views.Like.as_view(),
